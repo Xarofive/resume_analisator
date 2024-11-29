ee2b58aef81c8f8f676dfd2ae75a53d8e192ac9f
Admin <admin@kata.academy>
2024-11-29 21:35:54 +0300

    Normalize line endings

---
+++ src/main/java/webapp/resume_analizer/ResumeAnalizerApplication.java
@@ -0,0 +1,13 @@
+package webapp.resume_analizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeAnalizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResumeAnalizerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/webapp/resume_analizer/ResumeAnalizerApplicationTests.java
@@ -0,0 +1,13 @@
+package webapp.resume_analizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeAnalizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
