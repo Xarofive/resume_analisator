eeb6b6e606ebd4670df42c524b1f6ed654f07d6d
Хузин Ильдар <ildar.khuzin75@mail.ru>
2024-12-02 18:36:40 +0300

    сделал каркас resume-generator

---
+++ resume-generator/src/test/java/ru/kata/project/resumegenerator/ResumeGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kata.project.resumegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
