a1a0a4d7422f28ef76d78d71d568872cebcc815e
Хузин Ильдар <ildar.khuzin75@mail.ru>
2024-12-03 13:38:41 +0300

    добавил папки и Докерфайл

--- resume-generator/src/main/java/ru/kata/project/resumegenerator/ResumeGeneratorApplication.java
+++ resume-generator/src/main/java/ru/kata/project/resumegenerator/ResumeGeneratorApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ResumeGeneratorApplication {
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(ResumeGeneratorApplication.class, args);
 	}
